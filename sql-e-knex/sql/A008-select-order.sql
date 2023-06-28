57f243ee6ba3a8a00510bcd05d68c8cdd3999ff7
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-06-28 16:16:45 -0300

    Update

--- sql-e-knex/sql/A008-select-order.sql
+++
@@ -1,7 +0,0 @@
--- Order ordena valores:
--- order by id asc (id crescente)
--- order by id desc (id decrescente)
--- order by id asc, first_name desc (prioriza o id)
-select id, first_name, email as uemail from users
-where id BETWEEN 20 and 30
-order by first_name desc;  
---
+++ sql-e-knex/sql/A012-update.sql
@@ -0,0 +1,8 @@
+-- Update - Atualiza registros
+
+update users set first_name = 'Marinho',
+last_name = 'Peixão'
+where id BETWEEN 21 and 23;
+
+
+select * from users WHERE  id BETWEEN  21 and 23;
