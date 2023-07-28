5dc9c092bf3eac87275841f301711b54504c86e9
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-28 20:50:09 -0300

    performed first practice of the course

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/1_INTRO/hello-word/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/1_INTRO/hello-word/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1>Hello React</h1>
+      <h1>Hello React</h1>
+      <h2>Estou revisando conceitos de React</h2>
     </>
   )
 }
 
 export default App
+
+
