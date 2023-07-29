98efd277eee496343c26d807474387a512ba74ab
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-29 19:00:30 -0300

    36. The key property

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/3_AVANCANDO_NO_REACT/src/components/ListRender.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/3_AVANCANDO_NO_REACT/src/components/ListRender.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import { useState } from "react";
+
 
 const ListRender = () => {
     const [list] = useState(['Thiago', 'Lucas', 'Sisleide', 'Maria']);
 
+    const [users] = useState([
+        { id: 1, name: 'Thiago', age: 23 },
+        { id: 13, name: 'Sisleide', age: 60 },
+        { id: 1267, name: 'Lucas', age: 19 },
+    ]);
+
     return (
-        <ul>
-            {list.map((item) => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    )
-}
+        <div>
+            <ul>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.name} - {user.age}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ListRender
