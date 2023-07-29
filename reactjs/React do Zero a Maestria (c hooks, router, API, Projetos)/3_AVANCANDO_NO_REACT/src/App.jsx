4f7024b311287892419dd458f30625d13c9837cf
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-29 15:49:43 -0300

    31. Public Images in React

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/3_AVANCANDO_NO_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/3_AVANCANDO_NO_REACT/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
 
     <div className='App'>
       <h1>AVANCANDO NO REACT</h1>
+      <div>
+        <img src="src/img1.jpg" alt="developer" height='500px' width='500px'/>
+      </div>
     </div>
 
   )
