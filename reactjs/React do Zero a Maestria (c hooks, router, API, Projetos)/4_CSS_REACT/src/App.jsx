df16ed97cee6cfacbb2724978fe6743faadefcbf
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-30 15:09:58 -0300

    51. Component CSS

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
     <div className='App'>
       <h1>React with CSS</h1>
+      <MyComponent />
     </div>
 
 
