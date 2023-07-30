6acf3548af65a9612ecabd5d9a6a755d844848a8
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-30 15:14:25 -0300

    52. CSS inline

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
     <div className='App'>
       <h1>React with CSS</h1>
       <MyComponent />
+      <p>This paragraph is from APP.jsx</p>
+      <p style={{ color: 'blue', padding: '30px', borderTop: '4px solid red' }}>Este elemento foi estilizado inline</p>
     </div>
 
 
