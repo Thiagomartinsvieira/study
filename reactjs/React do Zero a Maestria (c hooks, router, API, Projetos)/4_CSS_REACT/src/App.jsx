df3e4831ee2dc34eff87fa455d77328df8358bcd
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-30 15:21:56 -0300

    53. Dynamic Inline CSS

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
@@ -1,8 +1,11 @@
 
+import { useState } from 'react';
 import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 10;
+  const [name] = useState('Thiago');
 
   return (
     <div className='App'>
@@ -10,6 +13,8 @@ function App() {
       <MyComponent />
       <p>This paragraph is from APP.jsx</p>
       <p style={{ color: 'blue', padding: '30px', borderTop: '4px solid red' }}>Este elemento foi estilizado inline</p>
+
+      <h2 style={name === 'Thiago' ? ({ color: 'green', backgroundColor: 'gold' }) : null}>CSS Dinamic</h2>
     </div>
 
 
