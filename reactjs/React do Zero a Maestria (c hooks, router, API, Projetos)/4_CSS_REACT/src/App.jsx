c0018f6450b41e02077ed226f796b637d61b269f
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-31 16:40:56 -0300

    54. Dynamic classes

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/4_CSS_REACT/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const n = 10;
   const [name] = useState('Thiago');
 
+  const redTitle = true;
+
   return (
     <div className='App'>
       <h1>React with CSS</h1>
@@ -15,6 +17,8 @@ function App() {
       <p style={{ color: 'blue', padding: '30px', borderTop: '4px solid red' }}>Este elemento foi estilizado inline</p>
 
       <h2 style={name === 'Thiago' ? ({ color: 'green', backgroundColor: 'gold' }) : null}>CSS Dinamic</h2>
+
+      <h2 className={redTitle ? 'red-title' : 'title'}>this title will have dynamic class</h2>
     </div>
 
 
