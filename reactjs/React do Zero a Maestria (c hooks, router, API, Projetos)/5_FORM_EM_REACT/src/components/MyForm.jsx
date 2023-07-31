661db3de34576ef9e92f4c80ded29dda84524407
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-31 18:46:17 -0300

    61. Inline state change

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/5_FORM_EM_REACT/src/components/MyForm.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/5_FORM_EM_REACT/src/components/MyForm.jsx
@@ -2,33 +2,44 @@ import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
-    // 3 - Gerenciamento de dados
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+  // 3 - Gerenciamento de dados
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
 
-    const handleName = (e) => {
-        setName(e.target.value)
-    }
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
 
-    console.log(name)
+  console.log(name)
+  console.log(email)
 
-    return (
+  return (
+    <div>
+      {/* 1 - criacao de form */}
+      <form>
         <div>
-            {/* 1 - criacao de form */}
-            <form>
-                <div>
-                    <label htmlFor=""></label>
-                    <input type="text" name='text' placeholder='Insert your name' onChange={handleName} />
-                </div>
-                {/* 2 - Label involving input */}
-                <label>
-                    <span>E-mail</span>
-                    <input type="email" name='email' placeholder='Insert your email addres' />
-                </label>
-                <input type="submit" />
-            </form>
+          <label htmlFor=""></label>
+          <input
+            type="text"
+            name="text"
+            placeholder="Insert your name"
+            onChange={handleName}
+          />
         </div>
-    )
+        {/* 2 - Label involving input */}
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Insert your email addres"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </div>
+  )
 }
 
 export default MyForm
