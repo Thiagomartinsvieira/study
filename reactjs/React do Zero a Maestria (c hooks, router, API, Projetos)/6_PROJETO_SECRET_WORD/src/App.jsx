4e95229825c7861c7bf934386654ae3f38c32c3c
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-01 18:30:56 -0300

    74. Choosing word and category at random

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/App.jsx
@@ -22,28 +22,65 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a radom category
+    const categories = Object.keys(words)
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word)
+
+    return { word, category }
+  }
+
   // starts the secret Game
   const startGame = () => {
-    setGameStage(stages[1].name)
-  }
+    // pick woed and pick categopry
+    const { word, category } = pickWordAndCategory();
 
-  // process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
-  }
+    // create an array of letters
+    let wordLetters = word.split('')
 
-  // restarts the game 
-  const retry = () => {
-    setGameStage(stages[0].name);
-  }
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-  return (
-    <div className='App'>
-      {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
-      {gameStage === 'end' && <GameOver retry={retry} />}
-    </div>
-  )
+  console.log(word, category)
+  console.log(wordLetters);
+
+  // fill states
+  setPickedWord(word)
+  setPickedCategory(category)
+  setLetters(letters)
+
+  setGameStage(stages[1].name)
+}
+
+// process the letter input
+const verifyLetter = () => {
+  setGameStage(stages[2].name);
+}
+
+// restarts the game 
+const retry = () => {
+  setGameStage(stages[0].name);
+}
+
+return (
+  <div className='App'>
+    {gameStage === 'start' && <StartScreen startGame={startGame} />}
+    {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+    {gameStage === 'end' && <GameOver retry={retry} />}
+  </div>
+)
 }
 
 export default App
