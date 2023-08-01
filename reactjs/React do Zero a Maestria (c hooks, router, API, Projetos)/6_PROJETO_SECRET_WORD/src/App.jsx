de905efd403b0ff353b53463953f619c32a3248b
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-01 19:20:30 -0300

    77. Consuming game states

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 
 // React
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 // Data
 import { wordsList } from './data/words'
@@ -19,12 +19,17 @@ const stages = [
 ]
 
 function App() {
-  const [gameStage, setGameStage] = useState(stages[0].name);
-  const [words] = useState(wordsList);
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
 
   const [pickedWord, setPickedWord] = useState('')
   const [pickedCategory, setPickedCategory] = useState('')
-  const [letters, setLetters] = useState([]);
+  const [letters, setLetters] = useState([])
+
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
 
   const pickWordAndCategory = () => {
     // pick a radom category
@@ -36,7 +41,7 @@ function App() {
 
     // pick a random word
     const word =
-      words[category][Math.floor(Math.random() * words[category].length)];
+      words[category][Math.floor(Math.random() * words[category].length)]
 
     console.log(word)
 
@@ -46,41 +51,52 @@ function App() {
   // starts the secret Game
   const startGame = () => {
     // pick woed and pick categopry
-    const { word, category } = pickWordAndCategory();
+    const { word, category } = pickWordAndCategory()
 
     // create an array of letters
     let wordLetters = word.split('')
 
-    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
 
-  console.log(word, category)
-  console.log(wordLetters);
+    console.log(word, category)
+    console.log(wordLetters)
 
-  // fill states
-  setPickedWord(word)
-  setPickedCategory(category)
-  setLetters(letters)
+    // fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters);
 
-  setGameStage(stages[1].name)
-}
+    setGameStage(stages[1].name)
+  }
 
-// process the letter input
-const verifyLetter = () => {
-  setGameStage(stages[2].name);
-}
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
 
-// restarts the game 
-const retry = () => {
-  setGameStage(stages[0].name);
-}
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
 
-return (
-  <div className='App'>
-    {gameStage === 'start' && <StartScreen startGame={startGame} />}
-    {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
-    {gameStage === 'end' && <GameOver retry={retry} />}
-  </div>
-)
+  return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wordLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === 'end' && <GameOver retry={retry} />}
+    </div>
+  )
 }
 
 export default App
--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
@@ -1,30 +1,56 @@
 import './Game.css'
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wordLetters,
+  guesses,
+  score,
+}) => {
   return (
-    <div className='game'>
-      <p className='points'>
-        <span>Score: 000</span>
+    <div className="game">
+      <p className="points">
+        <span>Score: {score}</span>
       </p>
-      <h1>guess the word: <span>Tip...</span></h1>
-      <h3 className='tip'>
-
-      </h3>
+      <h1>
+        guess the word: <span>{pickedCategory}...</span>
+      </h1>
+      <h3 className="tip"></h3>
+      <p>Voce ainda tem {guesses} tnentativa(s).</p>
       <div className="wordContainer">
-        <span className='letter'>A</span>
-        <span className='blankSquare'></span>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blakSquare"></span>
+          )
+        )}
       </div>
       <div className="letterContainer">
-        <p>Tente adivinha uma letra da palavra:</p>
-        <form action="">
-          <input type="text" name='letter' maxLength='1' required />
-          <button>Play</button>
+        <p>Tente adivnhar uma letra da palavra:</p>
+        <form>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            onChange={(e) => setLetter(e.target.value)}
+            required
+            value={letter}
+            ref={letterInputRef}
+          />
+          <button>Play!</button>
         </form>
       </div>
       <div className="wrongLettersContainer">
-        <p>Letras já ultilizadas:</p>
-        <span>a,</span>
-        <span>b,</span>
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
       </div>
     </div>
   )
