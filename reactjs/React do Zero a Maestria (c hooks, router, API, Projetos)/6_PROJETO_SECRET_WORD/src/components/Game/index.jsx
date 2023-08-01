ec5f8a9bf8a47cfc974054f6a024b329ecc63624
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-01 18:40:24 -0300

    75. Structuring game component

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
@@ -2,9 +2,30 @@ import './Game.css'
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar o jogo</button>
+    <div className='game'>
+      <p className='points'>
+        <span>Score: 000</span>
+      </p>
+      <h1>guess the word: <span>Tip...</span></h1>
+      <h3 className='tip'>
+
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className='blankSquare'></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinha uma letra da palavra:</p>
+        <form action="">
+          <input type="text" name='letter' maxLength='1' required />
+          <button>Play</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já ultilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
