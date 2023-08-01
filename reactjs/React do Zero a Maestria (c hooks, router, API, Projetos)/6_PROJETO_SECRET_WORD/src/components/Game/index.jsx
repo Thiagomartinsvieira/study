e3935b7e31f6ed7d2a63cf426b43348ded04b302
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-01 19:29:01 -0300

    78. Letter insertion event

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/6_PROJETO_SECRET_WORD/src/components/Game/index.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -10,6 +11,19 @@ const Game = ({
   guesses,
   score,
 }) => {
+  const [letter, setLetter] = useState('')
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter)
+
+    setLetter('')
+
+    letterInputRef.current.focus();
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -33,7 +47,7 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente adivnhar uma letra da palavra:</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             name="letter"
