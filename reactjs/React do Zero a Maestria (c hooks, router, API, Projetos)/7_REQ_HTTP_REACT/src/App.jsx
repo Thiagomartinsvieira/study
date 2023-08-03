23c6fb0bcb04140c29017fdd533a0c6a8827766b
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-03 10:37:12 -0300

    87. Retrieving data from the API

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const url = 'http://localhost:3000/products'
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+
 
   return (
     <div className="App">
       <h1>List of products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
