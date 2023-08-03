475676a1c075b37b5f5bbb73e34bb502ba7c1074
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-03 19:43:15 -0300

    challenge

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/App.jsx
@@ -1,105 +1,110 @@
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react'
-import useFetch from './hooks/useFetch'
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
+// 8 - errar url para mostrar erro
+// "http://localhost:3001/products"
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
-  // 4 - custom 
-  const { data: items, httpConfig, loading, error } = useFetch(url)
+  // 4 - custom hook e 5 - refactor post
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-
-  const url = 'http://localhost:3000/products'
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
+  // useEffect(async () => {
+  //   const res = await fetch("http://localhost:3000/products");
 
-      const data = await res.json();
+  //   const data = await res.json();
 
-      setProducts(data);
-    }
+  //   setProducts(data);
+  // }, []);
 
-    fetchData();
-  }, []);
-
-  // 2 - add products
+  // 2 - add product
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const product = {
       name,
       price,
     };
 
-    // const res = await fetch(url, {
-    //   method: 'POST',
+    // const res = await fetch("http://localhost:3000/products", {
+    //   method: "POST",
     //   headers: {
-    //     'content-type': 'application/json'
+    //     "Content-Type": "application/json",
     //   },
     //   body: JSON.stringify(product),
-    // })
+    // });
 
-
-    // // 3 - carregamento dinamico
     // const addedProduct = await res.json();
 
-    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+    // 3 - carregamento dinâmico
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
-    // 5 - refatorando post
+    // 5 - refatorar post
+    httpConfig(product, "POST");
 
-    httpConfig(product, "POST")
-
-    setName('')
-    setPrice('')
-
+    setName("");
+    setPrice("");
   };
 
+  /* 9 - desafio */
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  };
 
   return (
     <div className="App">
-      <h1>List of products</h1>
-      {/* 6 - Loading */}
-      {loading && <P>Carregando dados...</P>}
-      {error && <P>{error}</P>}
-      {!error && (
-        <ul>
-          {items && items.map((product) => (
+      <h1>Lista de produtos</h1>
+      {/* 6 - state de loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {items &&
+          items.map((product) => (
             <li key={product.id}>
-              {product.name} - R$ {product.price}
+              {product.name} - R$: {product.price}
+              {/* 9 - desafio */}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
             </li>
           ))}
-        </ul>
-      )}
+      </ul>
+
       <div className="add-product">
+        <p>Adicionar produto:</p>
         <form onSubmit={handleSubmit}>
           <label>
-            Name:
-            <input type="text"
+            Nome:
+            <input
+              type="text"
               value={name}
-              name='name'
+              name="name"
               onChange={(e) => setName(e.target.value)}
             />
           </label>
           <label>
-            Price:
-            <input type="number"
+            Preço:
+            <input
+              type="number"
               value={price}
-              name='price'
+              name="price"
               onChange={(e) => setPrice(e.target.value)}
             />
           </label>
           {/* 7 - state de loading no post */}
-          {loading && <input type="submit" disabled value='aguarde...' />}
-          {!loading && <input type="submit" value='create' />}
+          {loading ? <p>Aguarde!</p> : <input type="submit" value="Criar" />}
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/hooks/useFetch.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/7_REQ_HTTP_REACT/src/hooks/useFetch.jsx
@@ -1,80 +1,109 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
+// 4 - custom hook
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
 
+  // 5 - refatorando post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
 
-/// 4 - custom hook
+  // 6 - estado de loading
+  const [loading, setLoading] = useState(false);
 
-const useFetch = () => {
-    const [data, setDate] = useState(null)
+  // 8 - tratando erros
+  const [error, setError] = useState(false);
 
-    // 5 - refatorando o post
-    const [config, setConfig] = useState(null)
-    const [method, setMethod] = useState(null)
-    const [callFetch, setCallFetch] = useState(false)
+  // 9 - desafio
+  const [itemId, setItemId] = useState(null);
 
-    // 6 loading
-    const [loading, setLoading] = useState(false)
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    // 7 - tratando erros
+      setMethod("POST");
+    } else if (method === "DELETE") {
+      setConfig({
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const  [error, setError] = useState(null)
-
-    const httpConfig = (data, method) => {
-        if(method === "POST") {
-            setConfig({
-                method,
-                headers: {
-                    "content-type": 'aplication/json',
-                },
-                body: JSON.stringify(data)
-            })
-
-
-            setMethod(method);
-        }
+      setMethod("DELETE");
+      setItemId(data);
     }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchData = async () => {
+      // 6 - estado de loading
+      // mudar throttling no network p 3g
+      setLoading(true);
 
-        const fetchData = async () => {
+      // 8 - tratando erros
+      try {
+        const res = await fetch(url);
 
-            // 6 - loading
-            setLoading(true)
+        const json = await res.json();
 
-            try{
+        setData(json);
 
-            } catch (error) {
-                console.log(error.message)
-                setError("Houve um erro ao carregar os dados!")
-            }
+        setMethod(null);
 
-            setLoading(false)
-        }
+        // 8 - tratando erros
+        setError(null);
+      } catch (error) {
+        console.log(error.message);
 
-        fetchData()
-    }, [url, callFetch]);
+        setError("Houve um erro ao carregar os dados!");
+      }
 
-    // 5 - refaturando post
-    useEffect(() => {
-        const httpRequest = async = () => {
-            if (method === "POST") {
+      setLoading(false);
+    };
 
-                let fetchOptions = [url, config]
+    fetchData();
+  }, [url, callFetch]);
 
-                const res = await fetch(...fetchOptions)
+  // 5 - refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        // 7 - loading no post
+        // mudar throttling no network p 3g
+        setLoading(true);
 
-                const json = await res.json()
+        // 5 - refatorando post
+        let fetchOptions = [url, config];
 
-                setCallFetch(json)
+        const res = await fetch(...fetchOptions);
 
-            }
-        }
+        const json = await res.json();
 
-        httpRequest();
-    }, [config, method, url])
+        setCallFetch(json);
+        // 9 - desafio
+      } else if (method === "DELETE") {
+        const deleteUrl = `${url}/${itemId}`;
 
-    return { data, httpConfig, loading, error };
+        const res = await fetch(deleteUrl, config);
 
-}
+        const json = await res.json();
 
-export default useFetch
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config]);
+
+  console.log(config);
+
+  return { data, httpConfig, loading, error };
+};
