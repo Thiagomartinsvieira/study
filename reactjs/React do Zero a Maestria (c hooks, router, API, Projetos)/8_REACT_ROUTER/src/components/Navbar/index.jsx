4187e2fc5897a26f95d9b7445cd2252dd7afdc0a
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-03 20:34:08 -0300

    99. Navigation between pages

---
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/8_REACT_ROUTER/src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to='/about'>About</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
