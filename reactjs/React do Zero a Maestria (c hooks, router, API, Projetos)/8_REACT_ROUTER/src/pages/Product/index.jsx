bfd697a308fed7823697570f75f00a209ff0f677
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-03 21:15:34 -0300

    102. Dynamic data loading

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/8_REACT_ROUTER/src/pages/Product/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/8_REACT_ROUTER/src/pages/Product/index.jsx
@@ -1,12 +1,28 @@
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Product.css'
 
 const Product = () => {
 
     const { id } = useParams()
 
-    return <>
+    const url = 'http://localhost:3000/products' + id
+
+    const { data: product, loading, error } = useFetch(url)
+
+    console.log(product)
+
+    return
+    <>
         <p>Id do produto: {id} </p>
+        {error && <P>Ocorreu um erro...</P>}
+        {loading && <p>Carregando...</p>}
+        {product && (
+            <div>
+                <h1>{product.name}</h1>
+                <p>R$: {product.price}</p>
+            </div>
+        )}
     </>
 }
 
