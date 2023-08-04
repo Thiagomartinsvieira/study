db505f5a555b01b8386e5a2611d6c17e61f451bf
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-04 19:52:00 -0300

    07. URL redirection

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/8_REACT_ROUTER/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/8_REACT_ROUTER/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './components/Navbar'
@@ -24,6 +24,7 @@ function App() {
           <Route path='/products/:id' element={<Product />} />
           <Route path='/products/:id/info' element={<Info />} />
           <Route path='/search' element={<Search />} />
+          <Route path='/company' element={<Navigate to='/about' />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
