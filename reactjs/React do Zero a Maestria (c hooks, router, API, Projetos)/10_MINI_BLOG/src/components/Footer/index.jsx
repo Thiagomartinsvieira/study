2ec867e34af5bd96b3439dbbb9ad00acd7db557e
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-05 19:32:10 -0300

    124. CSS navbar and footer

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/components/Footer/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/components/Footer/index.jsx
@@ -1,8 +1,8 @@
-import styles from './Footer.css'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <h3>write about what you are interested in!</h3>
         <p>Mini Blog &copy; 2023</p>
     </footer>
--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/components/Nav/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/components/Nav/index.jsx
@@ -1,26 +1,26 @@
-import styles from './Nav.css'
+import styles from './Nav.module.css'
 import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
-  return (
-    <nav>
-        <NavLink to="/">
-            Mini <span>Blog</span>
-        </NavLink>
-        <ul>
-            <li>
-                <NavLink to='/'>
-                    Home
-                </NavLink>
-            </li>
-            <li>
-            <NavLink to='/about'>
-                    About
-                </NavLink>
-            </li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className={styles.navbar}>
+            <NavLink to="/" className={styles.brand}>
+                Mini <span>Blog</span>
+            </NavLink>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>
+                        About
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav
