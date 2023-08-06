83f25f3f74ff107537fe89299835c8eb1addd075
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-06 10:34:44 -0300

    131. Authentication context

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/App.jsx
@@ -7,24 +7,25 @@ import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
-
-
   return (
     <div className="App">
-      <BrowserRouter>
-      <Nav />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Nav />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
