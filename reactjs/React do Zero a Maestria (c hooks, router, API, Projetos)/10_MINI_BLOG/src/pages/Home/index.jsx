85aa94927748710c3fb9b26e1a2c268c89ca3345
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-09 20:30:10 -0300

    142. Structuring the home

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Home/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Home/index.jsx
@@ -1,11 +1,39 @@
-import './Home.module.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.css'
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const [query, setQuery] = useState('')
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className={StyleSheet.home}>
+      <h1>see our most recent posts</h1>
+      <form onSubmit={handleSubmit} className="search_form">
+        <input
+          type="text"
+          placeholder="or search by tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={StyleSheet.noposts}>
+            <p>No posts found</p>
+            <Link to="/posts/create" className="btn">
+              Create first Post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
