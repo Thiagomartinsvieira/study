72f2a7a6e4566841a70d804d00e65d7859ce8c1b
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-10 17:54:22 -0300

    144. Displaying posts on home page

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Home/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Home/index.jsx
@@ -1,36 +1,48 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+import PostDetail from '../../components/PostDetail'
 
 const Home = () => {
+  const { documents: posts, loading } = useFetchDocuments('posts')
+
+  const navigate = useNavigate()
+
   const [query, setQuery] = useState('')
-  const [posts] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (query) {
+      return navigate(`/search?q=${query}`)
+    }
   }
 
+  console.log(loading)
+
   return (
-    <div className={StyleSheet.home}>
-      <h1>see our most recent posts</h1>
-      <form onSubmit={handleSubmit} className="search_form">
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="or search by tags..."
+          placeholder="Ou busque por tags..."
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button className="btn btn-dark">Search</button>
+        <button className="btn btn-dark">Pesquisar</button>
       </form>
-      <div>
-        <h1>Posts...</h1>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
         {posts && posts.length === 0 && (
-          <div className={StyleSheet.noposts}>
-            <p>No posts found</p>
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
             <Link to="/posts/create" className="btn">
-              Create first Post
+              Criar primeiro post
             </Link>
           </div>
         )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
   )
