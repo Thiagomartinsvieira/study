9ff680ebe02b942ee425bb01cac7aef33002d594
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-11 20:27:06 -0300

    164. useEffect

---
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseEffect.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+
+const HookUseEffect = () => {
+    useEffect(() => {
+        console.log('Estou sendo Executado')
+    })
+
+    const [number, setNumber] = useState(1)
+
+    const changeSomething = () => {
+        setNumber(number + 1)
+    }
+
+    return (
+        <div>
+            <h2>useEffect</h2>
+            <p>Number: {number}</p>
+            <button onClick={changeSomething}>Executar</button>
+            <hr />
+        </div>
+    )
+}
+
+export default HookUseEffect
