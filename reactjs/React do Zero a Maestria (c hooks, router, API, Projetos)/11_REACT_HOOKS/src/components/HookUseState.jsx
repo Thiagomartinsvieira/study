32b7fcf65e0cf4eb24872a263892722c8efd2d2a
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-11 19:39:58 -0300

    161. useState e Inputs

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseState.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseState.jsx
@@ -10,21 +10,35 @@ const HookUseState = () => {
 
         setName('Thiago Martins')
 
-       
+
         console.log(userName)
-    } 
+    }
 
     console.log(name)
 
-  return (
-    <div>
-        <h2>useState</h2>
-        <p>Variable: {userName}</p>
-        <p>useState: {name}</p>
-        <button onClick={changeNames}>Mudar Nomes</button>
-        <hr />
-    </div>
-  )
+    const [age, setAge] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(age)
+    }
+
+    return (
+        <div>
+            <h2>useState</h2>
+            <p>Variable: {userName}</p>
+            <p>useState: {name}</p>
+            <button onClick={changeNames}>Mudar Nomes</button>
+            <p>Digite sua idade</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                <p>Vice tem {age} anos </p>
+            <input type="submit" value='enviar'/>
+            </form>
+            <hr />
+        </div>
+    )
 }
 
 export default HookUseState
