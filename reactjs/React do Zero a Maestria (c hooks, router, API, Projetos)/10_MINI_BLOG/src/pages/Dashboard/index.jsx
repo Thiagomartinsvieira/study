dc3f933cbad66e97f1091ce18b4b4cdb40fdd167
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-11 16:43:49 -0300

    151. Structuring dashboard

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Dashboard/index.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/10_MINI_BLOG/src/pages/Dashboard/index.jsx
@@ -1,9 +1,28 @@
 import styles from './Dashboard.module.css'
+import { useAuthValue } from '../../contexts/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
+  const { user } = useAuthValue()
+  const uid = user.id
+
+  const posts = []
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.nopost}>
+          <p>No posts found</p>
+          <Link to="/posts/create">Create First Post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Have Post</p>
+        </div>
+      )}
     </div>
   )
 }
