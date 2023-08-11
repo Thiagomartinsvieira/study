3fb4ad38449bebbc14751af80e7a15c68c5c402b
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-11 20:45:05 -0300

    166. Using the dependency array

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseEffect.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseEffect.jsx
@@ -12,15 +12,25 @@ const HookUseEffect = () => {
         setNumber(number + 1)
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log('serei executado apenas uma vez!')
     })
+
+    const [anotherNumer, setAnotherNumber] = useState(0)
+
+    useEffect(() => {
+        if (anotherNumer > 0) {
+            console.log("Sou executado apenas quando o another number muda")
+        }
+    }, [anotherNumer])
 
     return (
         <div>
             <h2>useEffect</h2>
             <p>Number: {number}</p>
             <button onClick={changeSomething}>Executar</button>
+            <p>Another Number! {anotherNumer}</p>
+            <button onClick={() => setAnotherNumber(anotherNumer + 1)}>mudar another number</button>
             <hr />
         </div>
     )
