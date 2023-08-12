0e2bf0d07e693eecffed5bbe27aec4a0061c42a8
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-12 10:03:58 -0300

    170. useRef e DOM

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseRef.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/components/HookUseRef.jsx
@@ -7,18 +7,34 @@ const HookUseRef = () => {
 
   useEffect(() => {
     numberRef.current = numberRef.current + 1;
-   
   })
+
+  const inputRef = useRef()
+  const [text, setText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setText('')
+
+    inputRef.current.focus()
+  } 
 
   return (
     <div>
-        <h2>useRef</h2>
-        <p>O component renderizou: {numberRef.current} vezes. </p>
-        <p>counter 1: {counter}</p>
-        <button onClick={() => setCounter(counter + 1)}>Counter A</button>
-        <p>counter 2: {counterB}</p>
-        <button onClick={() => setCounterB(counterB + 1)}>Counter B</button>
-        <hr />
+      <h2>useRef</h2>
+
+      <p>O component renderizou: {numberRef.current} vezes. </p>
+      <p>counter 1: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Counter A</button>
+      <p>counter 2: {counterB}</p>
+      <button onClick={() => setCounterB(counterB + 1)}>Counter B</button>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+        <input type="submit" value="Enviar" />
+      </form>
+      <hr />
     </div>
   )
 }
