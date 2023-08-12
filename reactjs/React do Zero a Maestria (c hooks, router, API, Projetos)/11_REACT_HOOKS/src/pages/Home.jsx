d01bb5ebffa10bb42301ba39ab9d050c48eb846c
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-12 15:13:25 -0300

    172. useMemo

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/pages/Home.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/11_REACT_HOOKS/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import HookUseState from '../components/HookUseState'
 import { SomeContext } from "../components/HookUseContext";
 import HookUseRef from '../components/HookUseRef'
 import HookUseCallback from '../components/HookUseCallback';
+import HookUseMemo from '../components/HookUseMemo';
 
 const Home = () => {
     const {contextValue} = useContext(SomeContext)
@@ -20,6 +21,7 @@ const Home = () => {
             <hr />
             <HookUseRef />
             <HookUseCallback />
+            <HookUseMemo />
         </div>
     )
 }
