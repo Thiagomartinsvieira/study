c14642c7bdaec32f064bdd9cee964d503e843875
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-19 17:28:18 -0300

    227. 227. Checking if user is authenticated

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+// hooks
+import { useAuth } from './hooks/useAuth'
 // components
 import Footer from './components/Footer'
 import Nav from './components/Nav'
@@ -11,18 +13,31 @@ import Home from './pages/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 
-
 function App() {
+  const { auth, loading } = useAuth()
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-        <div className='container'>
+        <div className="container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
         <Footer />
