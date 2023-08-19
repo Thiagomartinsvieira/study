83ce56ee89adfcc173f7f4d4e3e73688bb675bb1
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-18 21:00:52 -0300

    216. Global CSS

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/pages/Auth/Register.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/pages/Auth/Register.jsx
@@ -1,8 +1,34 @@
 import './Auth.css'
 
+// components 
+import { Link } from 'react-router-dom'
+
+// hooks
+import { useState, useEffect } from 'react'
+
+
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtutle">
+        Cadastre-se para ver as fotos dos seus amigos.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='confirme a seha' />
+        <input type="submit" value='Cadastrar' />
+      </form>
+      <p>
+        Já tem conta? <Link to='/login'>Clique Aqui.</Link>
+      </p>
+    </div>
   )
 }
 
