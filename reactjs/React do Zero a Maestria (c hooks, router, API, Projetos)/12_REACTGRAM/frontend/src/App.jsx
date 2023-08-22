19f242f6eb59ece8d8d58954080e3d50c049387e
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-22 18:12:46 -0300

    245. Creating photo post page

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/App.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 import EditProfile from './pages/EditProfile'
+import Profile from './pages/Profile'
 
 function App() {
   const { auth, loading } = useAuth()
@@ -34,6 +35,10 @@ function App() {
             <Route
               path="/profile"
               element={auth ? <EditProfile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/users/:id"
+              element={auth ? <Profile /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
