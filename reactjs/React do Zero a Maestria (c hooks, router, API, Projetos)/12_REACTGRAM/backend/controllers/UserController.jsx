85cf7a266cdae69b574009da1a6816feb69f8ba6
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-22 16:19:48 -0300

    234. Finishing login

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/backend/controllers/UserController.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/backend/controllers/UserController.jsx
@@ -1,153 +1,148 @@
-const User = require('../models/User.jsx');
+const User = require("../models/User");
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { default: mongoose } = require('mongoose');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
 const generateToken = (id) => {
     return jwt.sign({ id }, jwtSecret, {
-        expiresIn: '7d',
-    })
-}
+        expiresIn: "7d",
+    });
+};
 
-// Register user and sign in 
+// Register user and sign in
 const register = async (req, res) => {
-
-    const { name, email, password } = req.body
+    const { name, email, password } = req.body;
 
     // check if user exists
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
 
     if (user) {
-        res.status(422).json({ erros: ['Por favor ultilize outro email'] })
-        return
+        res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] });
+        return;
     }
 
     // Generate password hash
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
     // Create user
     const newUser = await User.create({
         name,
         email,
-        password: passwordHash
-    })
+        password: passwordHash,
+    });
 
-    // if user was created successfully, return the token
+    // If user was created sucessfully, return the token
     if (!newUser) {
-        res.status(422).json({ erros: ['Houve um erro, por favor tente mais tarde'] })
-        return
+        res.status(422).json({
+            errors: ["Houve um erro, por favor tente novamente mais tarde."],
+        });
+        return;
     }
 
     res.status(201).json({
         _id: newUser._id,
-        token: generateToken(newUser._id)
-    })
+        token: generateToken(newUser._id),
+    });
 };
 
-// sing user in
+// Get logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
+};
+
+// Sign user in
 const login = async (req, res) => {
+    const { email, password } = req.body;
 
-    const { email, password } = req.body
+    const user = await User.findOne({ email });
 
-    const user = await User.findOne({ email })
-
-    // check if user exists
+    // Check if user exists
     if (!user) {
-        res.status(404).json({ erros: ['Usuario não encontrado'] })
-        return
+        res.status(404).json({ errors: ["Usuário não encontrado!"] });
+        return;
     }
 
-    // check if password matches
+    // Check if password matches
     if (!(await bcrypt.compare(password, user.password))) {
-        res.status(422).json({ erros: ['Senha invalida.'] })
-        return
+        res.status(422).json({ errors: ["Senha inválida!"] });
+        return;
     }
 
     // Return user with token
-    res.status(201).json({
-        _id: User._id,
+    res.status(200).json({
+        _id: user._id,
         profileImage: user.profileImage,
-        token: generateToken(User._id)
-    })
-}
+        token: generateToken(user._id),
+    });
+};
 
-//get current logged in user 
-const getCurrentUser = async (req, res) => {
-    const user = req.user
-
-    res.status(200).json(user)
-}
-
-// update an user
+// Update user
 const update = async (req, res) => {
+    const { name, password, bio } = req.body;
 
-    const { name, password, bio } = req.body
-
-    let profileImage = null
+    let profileImage = null;
 
     if (req.file) {
-        profileImage = req.file.filename
+        profileImage = req.file.filename;
     }
 
-    const reqUser = req.user
+    const reqUser = req.user;
 
-    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select('-password')
+    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+        "-password"
+    );
 
     if (name) {
-        user.name = name
+        user.name = name;
     }
 
     if (password) {
-        const salt = await bcrypt.genSalt()
-        const passwordHash = await bcrypt.hash(password, salt)
-
-        user.password = passwordHash
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+        user.password = passwordHash;
     }
 
     if (profileImage) {
-        user.profileImage = profileImage
+        user.profileImage = profileImage;
     }
 
     if (bio) {
-        user.bio = bio
+        user.bio = bio;
     }
 
-    await user.save()
+    await user.save();
 
-    res.status(200).json(user)
-}
+    res.status(200).json(user);
+};
 
-// GEt user by id 
+// Get user by id
 const getUserById = async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    try {
-        const user = await User.findById(mongoose.Types.ObjectId(id).select('-password'))
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+        "-password"
+    );
 
-        // check if user exists
-        if (!user) {
-            res.status(404).json({ erros: ['Usuario não encontrado 2.'] })
-            return
-        }
-
-        res.status(200).json(user)
-
-    } catch (error) {
-        res.status(422).json({ erros: ['Usuario não encontrado.'] })
-        return
+    // Check if user exists
+    if (!user) {
+        res.status(404).json({ errors: ["Usuário não encontrado!"] });
+        return;
     }
 
-}
+    res.status(200).json(user);
+};
 
 module.exports = {
     register,
-    login,
     getCurrentUser,
+    login,
     update,
     getUserById,
-}
+};
