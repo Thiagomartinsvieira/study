71e11f05618aaf4bc9f26c2ac7318b9538d8c3ff
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-23 17:20:31 -0300

    267. Photo slice by id

--- reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/slices/photoSlice.jsx
+++ reactjs/React do Zero a Maestria (c hooks, router, API, Projetos)/12_REACTGRAM/frontend/src/slices/photoSlice.jsx
@@ -76,6 +76,13 @@ export const updatePhoto = createAsyncThunk(
   }
 )
 
+// Get photo by id
+export const getPhoto = createAsyncThunk('photo/getPhoto', async (id) => {
+  const data = await photoService.getPhoto(id)
+
+  return data
+})
+
 export const photoSlice = createSlice({
   name: 'photo',
   initialState,
@@ -166,6 +173,16 @@ export const photoSlice = createSlice({
         state.loading = false
         state.error = action.payload
         state.photo = {}
+      })
+      .addCase(getPhoto.pending, (state) => {
+        state.loading = true
+        state.error = false
+      })
+      .addCase(getPhoto.fulfilled, (state, action) => {
+        state.loading = false
+        state.success = true
+        state.error = null
+        state.photo = action.payload
       })
   },
 })
