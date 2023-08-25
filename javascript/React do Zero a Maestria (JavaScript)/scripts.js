bab267579366222273f280ae802d0881e838836e
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-25 18:10:49 -0300

    297. Destructuring

--- javascript/React do Zero a Maestria (JavaScript)/scripts.js
+++ javascript/React do Zero a Maestria (JavaScript)/scripts.js
@@ -131,3 +131,25 @@ const username = 'Thiago'
 const age = 30
 
 console.log(`Your user name is ${username} and he have ${age} `)
+
+// 6 Destructuring 
+const fruits = ['Apple', 'orange', 'limon']
+
+const [f1, f2, f3] = fruits
+
+console.log(f1)
+
+console.log(f2)
+
+console.log(f3)
+
+const productDetails = {
+    name: 'notebook',
+    price: 1000.99,
+    category: 'computer',
+    color: 'red'
+}
+
+const { name: productName, price, category: productCategory, color } = productDetails
+
+console.log(`O nome do produto é ${name}, custa R$${price}, pertence a categoria ${productCategory} e é da cor ${color}`)
