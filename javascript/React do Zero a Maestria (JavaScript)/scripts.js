20a2c6bfabd49374a1c41675e076526c21760cc5
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-25 17:41:30 -0300

    294. Filter method

--- javascript/React do Zero a Maestria (JavaScript)/scripts.js
+++ javascript/React do Zero a Maestria (JavaScript)/scripts.js
@@ -84,5 +84,28 @@ const user = {
     }
 }
 
-user.sayUserName()
-user.sayUserNameArrow()
+// user.sayUserName()
+// user.sayUserNameArrow()
+
+// 3 filter
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const highNumbers = arr.filter((n) => {
+    if(n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumbers)
+
+const users = [{name: 'Thiago', available: true}, {name: 'Lucas', available: true}, {name: 'Sisleide', available: true}, {name: 'Vitoria', available: false}]
+
+
+const availableUsers = users.filter((user) => user.available)
+const notAvailableUsers = users.filter((user) => !user.available)
+
+console.log(availableUsers)
+
+console.log(notAvailableUsers)
