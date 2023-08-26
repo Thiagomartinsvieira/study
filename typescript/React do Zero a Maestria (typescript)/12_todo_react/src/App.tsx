e7225ba2ec9d62f27d246fd9bf53a8e33da3ca82
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-26 19:17:29 -0300

    336. Early Styles

---
+++ typescript/React do Zero a Maestria (typescript)/12_todo_react/src/App.tsx
@@ -0,0 +1,21 @@
+// css
+import styles from './App.module.css'
+
+// components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {
+  
+  return (
+   <div>
+     <Header />
+      <main className={styles.main}>
+        <h1>Content...</h1>
+      </main>
+    <Footer />
+   </div>
+  )
+}
+
+export default App
