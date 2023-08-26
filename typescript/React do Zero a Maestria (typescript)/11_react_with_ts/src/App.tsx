98f9d13173fadee3968fe606b5236fae526614d9
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-26 17:34:34 -0300

    326. Destructuring props

--- typescript/React do Zero a Maestria (typescript)/11_react_with_ts/src/App.tsx
+++ typescript/React do Zero a Maestria (typescript)/11_react_with_ts/src/App.tsx
@@ -1,5 +1,13 @@
 
 import './App.css'
+// 6 - Desestruturando props
+import Destructuring from './components/Destructuring';
+
+// 4 - component import
+import FirstComponent from './components/FirstComponent';
+
+// 5 - Destructuring props
+import SecondComponent from './components/SecondComponent';
 
 function App() {
 
@@ -25,6 +33,13 @@ function App() {
           <p>Is not working</p>
         )}
         <h3>{userGreeting(name)}</h3>
+        <FirstComponent  />
+        <SecondComponent name='Second' />
+        <Destructuring title='Fist Post' 
+        content='content' 
+        commentsQty={10}  
+        tags={['ts', 'js']}
+        />
       </div>
     </div>
   );
---
+++ typescript/React do Zero a Maestria (typescript)/11_react_with_ts/src/components/FirstComponent.tsx
@@ -0,0 +1,15 @@
+// 3 components
+
+import React, { ReactElement } from 'react'
+
+
+
+const FirstComponent = (): ReactElement => {
+  return (
+    <div>
+        <h1>My First Component</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
