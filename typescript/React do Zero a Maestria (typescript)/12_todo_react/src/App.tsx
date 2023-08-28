68d45d7a26a32259636bf3fde700fcc22ad0ee44
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-28 16:59:41 -0300

    344. Removing task

--- typescript/React do Zero a Maestria (typescript)/12_todo_react/src/App.tsx
+++ typescript/React do Zero a Maestria (typescript)/12_todo_react/src/App.tsx
@@ -1,41 +1,48 @@
-import { useState, ChangeEvent, FormEvent, useEffect } from 'react'
+import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 
 // css
-import styles from './App.module.css'
+import styles from './App.module.css';
 
 // components
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import TaskForm from './components/TaskForm'
-import TaskList from './components/TaskList'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 // interfaces
-import { ITask } from './interfaces/Task'
+import { ITask } from './interfaces/Task';
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
 
-  const [taskList, setTaskList] = useState<ITask[]>([])
-  
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      }),
+    );
+  };
+
   return (
-   <div>
-     <Header />
+    <div>
+      <Header />
       <main className={styles.main}>
         <div>
           <h2>what will you do?</h2>
-          <TaskForm 
-          btnText='Create task'  
-          taskList={taskList}
-          setTaskList={setTaskList}
+          <TaskForm
+            btnText="Create task"
+            taskList={taskList}
+            setTaskList={setTaskList}
           />
         </div>
         <div>
           <h2>Your tasks:</h2>
-          <TaskList taskList={taskList}/>
+          <TaskList taskList={taskList} handleDelete={deleteTask} />
         </div>
       </main>
-    <Footer />
-   </div>
-  )
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- typescript/React do Zero a Maestria (typescript)/12_todo_react/src/components/TaskList.tsx
+++ typescript/React do Zero a Maestria (typescript)/12_todo_react/src/components/TaskList.tsx
@@ -1,37 +1,42 @@
-import React from 'react'
-
+import React from 'react';
 
 // interfaces
-import { ITask } from '../interfaces/Task'
+import { ITask } from '../interfaces/Task';
 
 // CSS
-import styles from './TaskList.module.css'
+import styles from './TaskList.module.css';
 
 type Props = {
-  taskList: ITask[]
-}
+  taskList: ITask[];
+  handleDelete(id: number): void
+};
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = ({ taskList, handleDelete }: Props) => {
   return (
     <>
-    {taskList.length > 0 ? (
-      taskList.map((task) => (
-        <div key={task.id} className={styles.task}>
-         <div className={styles.details}>
-          <h4> {task.title} </h4>
-          <p>Difficult: {task.difficult}</p>
-         </div>
-         <div className={styles.actions}>
-            <i className='bi bi-pencil'></i>
-            <i className='bi bi-trash'></i>
-         </div>
-        </div>
-      ))
-    ) : (
-      <p>Is not have registred tasks</p>
-    )}
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4> {task.title} </h4>
+              <p>Difficult: {task.difficult}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil"></i>
+              <i
+                className="bi bi-trash"
+                onclick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Is not have registred tasks</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
