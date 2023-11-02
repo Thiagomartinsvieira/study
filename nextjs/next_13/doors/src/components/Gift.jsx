878af5e2a45d9db468489e6b8f7be8a9ae3270b4
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-02 20:38:16 -0300

    Gift component

---
+++ nextjs/next_13/doors/src/components/Gift.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Gift.module.css'
+
+const Gift = () => {
+  return (
+    <div className={styles.gift}>
+        <div className={styles.top}></div>
+        <div className={styles.body}></div>
+        <div className={styles.ties}></div>
+        <div className={styles.ties2}></div>
+    </div>
+  )
+}
+
+export default Gift
