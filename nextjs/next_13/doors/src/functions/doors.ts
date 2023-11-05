b36f2cf7fda98b2a4bdb60cc9d8497983223f658
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-05 18:11:26 -0300

    Dealing with Array of Ports

---
+++ nextjs/next_13/doors/src/functions/doors.ts
@@ -0,0 +1,21 @@
+import DoorModel from "../model/door"
+
+ export default function createDoors(qtd: number, selected: number): DoorModel[]{
+    return Array.from({length: qtd}, (_, i) => {
+        const number = i + 1
+        const haveGift = number === selected
+        return new DoorModel(number, haveGift)
+    })
+ }
+
+ export function updateDoors(doors: DoorModel[],doorModified: DoorModel): DoorModel[] {
+    return doors.map(currentDoor => {
+        const samAsModified = currentDoor.number === doorModified.number
+
+        if(samAsModified) {
+            return doorModified
+        } else {
+           return doorModified.openDoor ? currentDoor :  currentDoor.markOff()
+        }
+    })
+ }
--- nextjs/next_13/doors/src/pages/index.tsx
+++ nextjs/next_13/doors/src/pages/index.tsx
@@ -1,14 +1,22 @@
 import DoorModel from "../model/door";
 import Door from "../components/Door";
 import { useState } from "react";
+import createDoors, { updateDoors } from "../functions/doors";
 
 
 export default function Home() {
-  const [d1, setD1] = useState(new DoorModel(1))
+  const [doors, setDoors] = useState(createDoors(3, 2))
+
+  function renderDoors() {
+    return doors.map(door => {
+      return <Door key={door.number} value={door} 
+      onchange={newDoor => setDoors(updateDoors(doors, newDoor))} />
+    })
+  }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Door value={d1} onchange={newDoor => setD1(newDoor)} />
+    <div style={{ display: 'flex' }}>
+      {renderDoors()}
     </div>
   )
 }
