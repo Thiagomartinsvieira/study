d1228419819cee2541bfe1f6f803cd0d0b3b67b7
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-07 11:39:18 -0300

    Usando Router

--- nextjs/next_13/doors/src/functions/doors.ts
+++ nextjs/next_13/doors/src/functions/doors.ts
@@ -1,9 +1,9 @@
 import DoorModel from "../model/door"
 
- export default function createDoors(qtd: number, selected: number): DoorModel[]{
+ export default function createDoors(qtd: number, DoorWithGift: number): DoorModel[]{
     return Array.from({length: qtd}, (_, i) => {
         const number = i + 1
-        const haveGift = number === selected
+        const haveGift = number === DoorWithGift
         return new DoorModel(number, haveGift)
     })
  }
--- nextjs/next_13/doors/src/pages/game/[doors]/[haveGift].tsx
+++ nextjs/next_13/doors/src/pages/game/[doors]/[haveGift].tsx
@@ -1,12 +1,20 @@
-import styles from '../styles/Game.module.css'
-import React, { useState } from 'react'
-import Door from '../components/Door'
-import createDoors, { updateDoors } from '../functions/doors'
+import styles from '../../../styles/Game.module.css'
+import React, { useEffect, useState } from 'react'
+import Door from '../../../components/Door'
+import createDoors, { updateDoors } from '../../../functions/doors'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const game = () => {
+    const router = useRouter()
 
-    const [doors, setDoors] = useState(createDoors(5, 3))
+    const [doors, setDoors] = useState(createDoors([]))
+
+    useEffect(() => {
+      const doors = +router.query.doors
+      const haveGift = +router.query.haveGift
+      setDoors(createDoors(doors, haveGift))
+    }, [router?.query])
 
     function renderDoors() {
       return doors.map(door => {
