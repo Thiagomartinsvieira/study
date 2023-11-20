f8240d8a311de821b79116edb1034545025978b2
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-11-20 09:08:03 -0300

    move to next 14

--- nextjs/next_13/rendering/src/pages/api/students/[id].js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    const id = +req.query.id
-    res.status(200).json({
-        id,
-        name: `Thiago Martins ${id}`,
-        email: `thiago${id}@email.com`
-    })
-}
