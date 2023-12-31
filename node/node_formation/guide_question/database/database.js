9c87c9f97af684c284dc25a48e3d4ec883f62920
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2023-12-31 10:30:08 -0300

    database connection

---
+++ node/node_formation/guide_question/database/database.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const connection = new Sequelize('guide_questions', 'node_user', '1234', {
+    host: 'localhost',
+    dialect: 'mysql', 
+});
+
+module.exports = connection;
--- node/node_formation/guide_question/index.js
+++ node/node_formation/guide_question/index.js
@@ -1,22 +1,41 @@
 const express = require('express'); 
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database')
+// Database 
 
-// i speaking for the express using the ejs with view engine
+connection
+    .authenticate()
+    .then(() => {
+        console.log('connection made to database')
+    })
+    .catch((errMsg) => {
+        console.log(errMsg)
+    })
+
+// Set view engine and static folder
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/',(req, res) => {
-    res.render("index");
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index'); 
 });
 
 app.get('/to_ask', (req, res) => {
-    res.render('to_ask');
+    res.render('to_ask'); 
 })
 
 app.post('/savequestion', (req, res) => {
-    res.send('Form received!');
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send('Form received! title 2' + title + ' ' + 'description' + description)
 });
 
 app.listen(8080, () => {
-    console.log('app running!');
+    console.log('App running on http://localhost:8080');
 });
