56879688444ee23a58812f00d11f585a247e72ad
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-11 17:15:34 -0300

    Destructuring

--- node/node_formation/JavaScript ES6/script.js
+++ node/node_formation/JavaScript ES6/script.js
@@ -1,37 +1,13 @@
-var name = 'Thiago Martins' // Global 
-let name2 = 'Lucas' // Global
-// Global: Everyone can use
-// Local: only the function where it was declared can use it
-
-// Var only has two scopes: Global and Local
-// Let too have two scopes: Global and Local, block
-
-
-function func1(){
-    var lastName = 'Vieira' // local
-    let lastName2 = 'Vieirinha' // Local
-    console.log("oi " + name + " " + lastName)
-    console.log('Oi '+ name2 + " " + lastName2)
+var user = {
+    name: 'Thiago Martins',
+    prof: 'Developer',
+    companny: 'Sonda IT',
+    Curse: 'Node.JS Formation'
 }
 
-function func2(){
-    console.log("olá " + name)
-}
+var {name, prof, Curse, companny} = user;
 
-func1();
-func2();
 console.log(name)
-console.log(name2)
-console.log("------------------------------------------------------------");
-
-var a = 10; // Global
-let b = true; 
-
-if(b){
-    var c = 20 // Block
-    console.log(a + c);
-} else {
- console.log('is False')
-}
-
-console.log(a + c)
+console.log(prof)
+console.log(companny)
+console.log(Curse)
