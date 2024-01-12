ceeb197c873a2153abdc16170646a147232e6d0e
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-12 11:12:14 -0300

    Async e Await

--- node/node_formation/asynchronous programming - Promises and async - Await/async/promises.js
+++ node/node_formation/asynchronous programming - Promises and async - Await/async/promises.js
@@ -1,4 +1,4 @@
-function toTakeId(){
+function getId(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(5)
@@ -18,7 +18,7 @@ function sendEmail(body, to){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             
-            const thereWasError = false
+            const thereWasError = true
 
             if(!thereWasError){
                     resolve({time: 6, to: "thiago@email.com"}) // Promisse Ok!
@@ -30,15 +30,45 @@ function sendEmail(body, to){
     });
 }
 
-console.log('Start!')
-toTakeId().then((id) => {
-    searchEmailInDatabase(id).then((email) => {
-        sendEmail('Hello', "how are you?", email).then(() => {
-            console.log("Email, ok " + id)
-        }).catch(error => {
-            console.log(error)
-        })
-
+function catchUsers(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([
+                {name: "Thiago", lang: 'JS'},
+                {name: "Lucas", lang: 'Rust'},
+                {name: "Maria", lang: 'Java'},
+            ])
+        }, 3000)
     })
-})
-console.log('Finish')
+}
+
+async function main(){
+    var id = await getId() 
+    var email = await searchEmailInDatabase(id)
+    try {
+        await sendEmail('Hello', 'How are you', email)
+        console.log('Email send with success!')
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+main()
+
+
+
+
+
+// console.log('Start!')
+// toTakeId().then((id) => {
+//     searchEmailInDatabase(id).then((email) => {
+//         sendEmail('Hello', "how are you?", email).then(() => {
+//             console.log("Email, ok " + id)
+//         }).catch(error => {
+//             console.log(error)
+//         })
+
+//     })
+// })
+// console.log('Finish')
