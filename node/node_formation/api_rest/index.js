0aba27fa4666f5119ed5239b6d6bd268030db6b9
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-15 20:53:19 -0300

    consuming api with axios

--- node/node_formation/api_rest/index.js
+++ node/node_formation/api_rest/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app =  express();
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -8,8 +11,8 @@ app.use(bodyParser.json())
 var DB = {
     games: [
         {
-            id: 23,
-            title: 'Bomba Patch',
+            id: 13,
+            title: 'Bomba Patch 2024',
             year: 2024,
             price: 10
         },
@@ -29,11 +32,93 @@ var DB = {
 
 }
 
+
 app.get('/games', (req, res) => {
     res.statusCode = 200; 
     res.json(DB.games)
 })
 
-app.listen(45678, () => {
+app.get('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    } else{
+        
+        let id = parseInt(req.params.id)
+
+        let game = DB.games.find(g => g.id == id)
+
+        if(game != undefined){
+            res.statusCode = 200
+            res.json(game)
+        }else {
+            res.sendStatus(404)
+        }
+    }
+})
+
+// Register one game
+app.post("/game", (req, res) => {
+    let {title, price, year} = req.body
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200)
+
+})
+
+app.delete("/game/:id", (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    } else{    
+        let id = parseInt(req.params.id)
+        let index = DB.games.findIndex(g => g.id == id)
+
+        if(index == -1){
+            res.sendStatus(404)
+        }else{
+            DB.games.splice(index, 1)
+            res.sendStatus(200)
+        }
+    }
+})
+
+app.put('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    } else{
+        
+        let id = parseInt(req.params.id)
+
+        let game = DB.games.find(g => g.id == id)
+
+        if(game != undefined){
+            
+            var {title, price, year} = req.body
+
+            if(title != undefined){
+                game.title = title
+            }
+
+            if(price != undefined){
+                game.price = price
+            }
+
+            if(year != undefined){
+                game.year = year
+            }
+
+            res.sendStatus(200)
+
+        }else {
+            res.sendStatus(404)
+        }
+    }
+})
+
+app.listen(8080, () => {
     console.log('API is Runnig!')
 })
