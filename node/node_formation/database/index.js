1c943478261fd6a94cdd6e29394636b9b634a5ce
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-22 08:44:06 -0300

    Many-to-many relationship

--- node/node_formation/database/index.js
+++ node/node_formation/database/index.js
@@ -53,3 +53,78 @@ var database = require('./database')
 // }).catch(error => {
 //     console.log(error)
 // })
+
+// database.where({id: 6}).delete().table('games').then(data => {
+//     console.log(data)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+// database.where({id: 2}).update({name: 'WWE SVR11'}).table('games').then(data => {
+//     console.log(data)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+// database.select().table('games').orderBy('name', 'asc').then(data => {
+//     console.log(data)
+// }).then(error => {
+//     console.log(error)
+// })
+
+// database.insert({
+//     name: 'Rockstar',
+//     game_id: 4
+// }).table('studios').then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+// database.select(['games.*', 'studios.name as studio_name'])
+// .table('games')
+// .innerJoin('studios', 'studios.game_id', 'games.id')
+// .where('games.id', 4)
+// .then(data => {
+//     console.log(data)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+// 1 to many
+ 
+// database.select(['games.*', 'studios.name as studio_name'])
+// .table('games')
+// .innerJoin('studios', 'studios.game_id', 'games.id')
+// .then(data => {
+//     var studiosGamesArray = data
+//     var game = {
+//         id: 0,
+//         name: '',
+//         studios: []
+//     }
+
+//     game.id = data[0].id
+//     game.name = data[0].name
+
+//     data.forEach(studio => {column_3
+//         game.studios.push({name: studio.studio_name})
+//     })
+//     console.log(game)
+
+// }).catch(error => {
+//     console.log(error)
+// })
+
+database.select([
+    "studios.name as studio_name",
+    "games.name as game_name",
+    "games.price"
+]).table('games_studios')
+.innerJoin('games', 'games.id', '=', 'games_studios.game_id') 
+.innerJoin('studios', 'studios.id', '=', 'games_studios.studio_id') 
+.where('games.id', 4).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.log(error);
+});
