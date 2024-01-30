941245efddcffceabab0004b97a677a4f740177c
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-01-30 06:42:34 -0300

    events

--- vuejs/node_formation/guide_clients/src/components/Client.vue
+++ vuejs/node_formation/guide_clients/src/components/Client.vue
@@ -1,34 +1,41 @@
 <template>
-    <div :class="{'client' : !isPremium, 'client-premium' : isPremium}">
+    <div :class="{ 'client': !isPremium, 'client-premium': isPremium }">
         <h4>Name: {{ client.name }}</h4>
         <hr>
         <p>Email: {{ client.email }}</p>
-        <p v-if="showAge === false">Age: {{ client.age }}</p>
-        <p v-else>The user hide age</p>
+        <p v-if="!showAge">Age: {{ client.age }}</p>
+        <p v-else>The user has hidden their age.</p>
+        <button @click="changeColor">Change color!</button>
     </div>
 </template>
 
 <script setup>
-const isPremium = false
-const {props} = defineProps(['client', 'showAge'])
+import { defineProps } from 'vue';
+
+const { props } = defineProps(['client', 'showAge']);
+
+let isPremium = false;
+
+const changeColor = ($event) => {
+    console.log($event)
+    isPremium = !isPremium;
+}
 </script>
 
 <style scoped>
-.client {
-    background-color: #ece5e3;
+.client, .client-premium {
     max-width: 700px;
     height: 200px;
     padding: 1%;
     margin-top: 4%;
+}
+
+.client {
+    background-color: #ece5e3;
 }
 
 .client-premium {
     background-color: black;
     color: yellow;
-    max-width: 700px;
-    height: 200px;
-    padding: 1%;
-    margin-top: 4%;
 }
 </style>
- 
