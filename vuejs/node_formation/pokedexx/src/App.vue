eb51072f97eb612c9be5f52b7687d3e3acf221ff
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-01 12:12:22 -0300

    http requests

---
+++ vuejs/node_formation/pokedexx/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+   
+  </div>
+  
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const pokemons = ref([])
+
+const created = async() => {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    
+    pokemons.value = response.data.results
+  } catch (error) {
+    console.log('Error fetching data', error)
+  }
+}
+</script>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
