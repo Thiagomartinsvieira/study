20ebde531cfbc12c3af6df83b33f197c9ca27eb7
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-01 10:49:08 -0300

    Event broadcast

--- vuejs/node_formation/guide_clients/src/App.vue
+++ vuejs/node_formation/guide_clients/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <div v-for="(client, index) in clients" :key="client.id">
-      <h1>{{index + 1}}</h1>
-      <Client :client="client" />
-      <hr>
-      <h2>Edit: </h2>
-      <input type="text" v-model="client.name">
-      <input type="text" v-model="client.email">
-    </div>
-
+    <h2>Register: </h2>
+    <small id="nameError" v-show="thereIsAnError">The name is invalid, try again</small><br>
+    <input type="text" placeholder="name" v-model="nameField"> <br>
+    <input type="email" placeholder="email" v-model="emailField"><br>
+    <input type="number" placeholder="age" v-model="ageField"><br>
+    <button @click="registerUser">Register</button>
+    <hr>
+    <Client v-for="(client, index) in clients" :key="client.id" :client="client" @onMeDelete="deleteUser(client.id)" />
   </div>
 </template>
 
@@ -16,46 +15,51 @@
 import { ref } from 'vue';
 import Client from './components/Client.vue';
 
-const thiagoClient = ref({
-  name: "Thiago Vieira",
-  email: 'thiago@email.com',
-  age: 24
-});
-const clients = [
+const nameField = ref('');
+const emailField = ref('');
+const ageField = ref(0);
+const thereIsAnError = ref(false);
+
+const clients = ref([
   {
-    id:1,
+    id: 1,
     name: 'Lucas Vieira',
     email: 'lucas@email.com',
     age: 20
   },
-   {
-    id:2,
+  {
+    id: 2,
     name: 'Sisleide Costa',
     email: 'sisleide@email.com',
     age: 60
-  },
-   {
-    id:3,
-    name: 'Camilo Durval',
-    email: 'camilo@email.com',
-    age: 26
-  },
-   {
-    id:4,
-    name: 'Roberta Souza',
-    email: 'roberta@email.com',
-    age: 21
-  },
-   {
-    id:5,
-    name: 'Michael Scott',
-    email: 'michael@email.com',
-    age: 47
-  },
-  
-]
+  }
+]);
+
+const registerUser = () => {
+  if (nameField.value === '' || nameField.value.length < 3) {
+    thereIsAnError.value = true;
+  } else {
+    clients.value.push({
+      id: Date.now(),
+      name: nameField.value,
+      email: emailField.value,
+      age: ageField.value
+    });
+    thereIsAnError.value = false;
+  }
+
+  nameField.value = '';
+  emailField.value = '';
+  ageField.value = 0;
+};
+
+const deleteUser = (clientId) => {
+  console.log('Received event from child component with ID:', clientId);
+};
 </script>
 
-<style>
-
+<style scoped>
+#nameError {
+  color: red;
+}
 </style>
--- vuejs/node_formation/guide_clients/src/components/Client.vue
+++ vuejs/node_formation/guide_clients/src/components/Client.vue
@@ -1,41 +1,54 @@
 <template>
-    <div :class="{ 'client': !isPremium, 'client-premium': isPremium }">
-        <h4>Name: {{ client.name }}</h4>
-        <hr>
-        <p>Email: {{ client.email }}</p>
-        <p v-if="!showAge">Age: {{ client.age }}</p>
-        <p v-else>The user has hidden their age.</p>
-        <button @click="changeColor">Change color!</button>
-    </div>
+  <div :class="{ 'client': !isPremium, 'client-premium': isPremium }">
+    <h4>Name: {{ client.name }}</h4>
+    <hr>
+    <p>Email: {{ client.email }}</p>
+    <p v-if="!showAge">Age: {{ client.age }}</p>
+    <p v-else>The user has hidden their age.</p>
+    <button @click="changeColor">Change color!</button>
+    <button @click="issueEventDelete">Delete</button>
+  </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, getCurrentInstance, ref } from 'vue';
 
 const { props } = defineProps(['client', 'showAge']);
+const instance = getCurrentInstance();
 
-let isPremium = false;
+const isPremium = ref(false);
 
-const changeColor = ($event) => {
-    console.log($event)
-    isPremium = !isPremium;
+const changeColor = () => {
+  isPremium.value = !isPremium.value;
+}
+
+const issueEventDelete = () => {
+  console.log('Issue by children');
+  if (instance) {
+    instance.emit('onMeDelete', { clientId: props.client.id, course: 'Formation Node.js', onSale: true });
+  }
+}
+
+const test = () => {
+  console.log('testing for real')
+  alert('This is an alert')
 }
 </script>
 
 <style scoped>
 .client, .client-premium {
-    max-width: 700px;
-    height: 200px;
-    padding: 1%;
-    margin-top: 4%;
+  max-width: 700px;
+  height: 200px;
+  padding: 1%;
+  margin-top: 4%;
 }
 
 .client {
-    background-color: #ece5e3;
+  background-color: #ece5e3;
 }
 
 .client-premium {
-    background-color: black;
-    color: yellow;
+  background-color: black;
+  color: yellow;
 }
 </style>
