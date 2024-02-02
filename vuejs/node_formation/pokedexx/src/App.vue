318a6650b8361610f5332b23ad4a85767fad10da
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-02-02 07:23:59 -0300

    created poke component

--- vuejs/node_formation/pokedexx/src/App.vue
+++ vuejs/node_formation/pokedexx/src/App.vue
@@ -1,39 +1,36 @@
 <template>
   <div>
-   
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+    </div>
   </div>
-  
 </template>
 
 <script setup>
-import axios from 'axios'
-import { ref } from 'vue'
+import axios from 'axios';
+import Pokemon from './components/Pokemon.vue';
+import { ref } from 'vue';
 
-const pokemons = ref([])
+const pokemons = ref([]);
 
-const created = async() => {
+const created = async () => {
   try {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-    
-    pokemons.value = response.data.results
+    console.log('Requesting data from API...');
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+    console.log('Data received:', response.data.results);
+
+    pokemons.value = response.data.results;
   } catch (error) {
-    console.log('Error fetching data', error)
+    console.log('Error fetching data', error);
   }
-}
+};
+
+const upper = (value) => {
+  if (!value) return '';
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
 </script>
 
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ vuejs/node_formation/pokedexx/src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ formattedName }}</h1>
+    <small>{{ url }}</small>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  num: Number,
+  name: String,
+  url: String
+});
+
+const formattedName = formatName(props.name);
+
+function formatName(name) {
+  return name ? name.charAt(0).toUpperCase() + name.slice(1) : '';
+}
+</script>
+
+<style>
+
+</style>
