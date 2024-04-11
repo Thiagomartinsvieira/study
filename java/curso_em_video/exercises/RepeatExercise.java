5278177509957d1947fb52aa421efaa43db54526
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-11 06:10:09 -0300

    repetition exercise

---
+++ java/curso_em_video/exercises/RepeatExercise.java
@@ -0,0 +1,44 @@
+package exercises;
+
+import javax.swing.JOptionPane;
+
+public class RepeatExercise {
+    public static void main(String[] args) {
+        int n;
+        int totalNumbers = 0;
+        int sum = 0;
+        int evenCount = 0;
+        int oddCount = 0;
+        int above100Count = 0;
+        double average;
+        do {
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "<html>Enter a number: <br><em>(0 value stpos)<em></html>"));
+
+            if(n != 0){
+                sum += n;
+                totalNumbers++;
+
+                if(n % 2 == 0) evenCount++;
+                else oddCount++;5
+
+                if(n > 100) above100Count++;
+            }
+
+        } while (n != 0);
+
+        if(totalNumbers > 0){
+            average = (double) sum / totalNumbers;
+        } else {
+            average = 0;
+        }
+
+        JOptionPane.showMessageDialog(null, "<html>Final Result <br> <hr>" +
+                "Total Numbers: " + totalNumbers + "<br> " +
+                "Sum: " + sum + "<br> " +
+                "Even Numbers: " + evenCount + "<br> " +
+                "Odd Numbers: " + oddCount + "<br> " +
+                "Numbers above 100: " + above100Count + "<br> " +
+                "Average: " + String.format("%.2f", average) + "</html>");
+
+    }
+}
