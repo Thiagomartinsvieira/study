1f2909b422569da6d375dc94c8869c6dac7e338c
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-22 09:53:10 -0300

    first program - Java

---
+++ java/cod3r/Fundamentals/src/Fundamentals/FirstProgram.java
@@ -0,0 +1,10 @@
+package Fundamentals;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("First program part #01 !");
+        System.out.println("First program part #02 !");
+
+    }
+}
