9c4a33d5bd4a0a52818b7d25f50e8fc2d080663c
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-23 10:25:58 -0300

    logical operators

---
+++ java/cod3r/Fundamentals/src/Fundamentals/ArithmeticChanllenge.java
@@ -0,0 +1,22 @@
+package Fundamentals;
+
+public class ArithmeticChanllenge {
+    public static void main(String[] args) {
+        double numA = Math.pow(6 * (3 + 2), 2);
+        double denA = 3 * 2;
+
+        double numB = (1 - 5) * (2 - 7);
+        double denB = 2;
+
+        double higherA = numA / denA;
+        double higherB = Math.pow(numB / denB, 2);
+
+        double hegher = Math.pow(higherA - higherB, 3);
+        double bottom = Math.pow(10, 3);
+
+        double result = hegher / bottom;
+
+        System.out.println("The result is " + result);
+
+    }
+}
