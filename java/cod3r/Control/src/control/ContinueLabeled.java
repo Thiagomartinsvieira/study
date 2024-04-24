2f00d2a21b22fcad71b932c25c56ee7b533098df
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-24 12:05:33 -0300

    Continue

---
+++ java/cod3r/Control/src/control/ContinueLabeled.java
@@ -0,0 +1,17 @@
+package control;
+
+public class ContinueLabeled {
+    public static void main(String[] args) {
+        external: for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if(i == 1) {
+                    continue external;
+                }
+                System.out.printf("[%d %d]", i, j);
+            }
+            System.out.println();
+        }
+        System.out.println("Finish!");
+    }
+}
