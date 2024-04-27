384d8972eaffb3a0673982c079a15941a2285590
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-27 11:01:57 -0300

    Composition challenge

---
+++ java/cod3r/Exercises/src/oo/composition/challenge/ClientTest.java
@@ -0,0 +1,19 @@
+package oo.composition.challenge;
+public class ClientTest {
+    public static void main(String[] args) {
+
+        Purchase purchase1 = new Purchase();
+        purchase1.addItem("Pen", 7.54, 100);
+        purchase1.addItem("Notebook", 3275.83, 2);
+
+        Purchase purchase2 = new Purchase();
+        purchase2.addItem("Book", 20, 4);
+        purchase2.addItem("Printer", 1100.23, 1);
+
+        Client client = new Client("Maria julia Moraes");
+        client.addPurchase(purchase1);
+        client.purchases.add(purchase2);
+
+        System.out.println(client.getTotalValue());
+    }
+}
---
+++ java/cod3r/Exercises/src/oo/composition/challenge/Item.java
@@ -0,0 +1,12 @@
+package oo.composition.challenge;
+
+public class Item {
+
+    final Product product;
+    final int amout;
+
+    Item(Product product, int amount){
+        this.product = product;
+        this.amout = amount;
+    }
+}
