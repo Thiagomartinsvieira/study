8d9f96081ae809128bd4dc5b422a47423de12430
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-04-30 20:10:27 -0300

    Filter

---
+++ java/cod3r/Exercises/src/streams/Student.java
@@ -0,0 +1,13 @@
+package streams;
+
+public class Student {
+    final String name;
+    final double grade;
+    final boolean goodBehavior;
+
+    public Student(String name, double grade, boolean goodBehavior) {
+        this.name = name;
+        this.grade = grade;
+        this.goodBehavior = goodBehavior;
+    }
+}
---
+++ java/cod3r/Exercises/src/streams/challenge/CarDealership.java
@@ -0,0 +1,15 @@
+package streams.challenge;
+
+public class CarDealership {
+    final String car;
+    final String segment;
+    final int year;
+    final boolean used;
+
+    public CarDealership(String car, String segment, int year, boolean used) {
+        this.car = car;
+        this.segment = segment;
+        this.year = year;
+        this.used = used;
+    }
+}
