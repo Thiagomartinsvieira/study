985f3dde580e77d6851dd9552845c1b408fe46da
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-05-01 15:54:14 -0300

    Stream API

--- java/cod3r/Exercises/src/streams/Student.java
+++ java/cod3r/Exercises/src/streams/Student.java
@@ -1,5 +1,7 @@
 package streams;
 
+import java.util.Objects;
+
 public class Student {
     final String name;
     final double grade;
@@ -13,5 +15,22 @@ public class Student {
         this.name = name;
         this.grade = grade;
         this.goodBehavior = goodBehavior;
+    }
+
+    public String toString(){
+        return name + " has a rating " + grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Double.compare(grade, student.grade) == 0 && goodBehavior == student.goodBehavior && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, grade, goodBehavior);
     }
 }
