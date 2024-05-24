ff3c3960681fc12fd4af222cd5f08e5c4142e235
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-05-24 20:38:08 -0300

    JavaFx

---
+++ java/cod3r/exercises-javafx/src/main/java/org/example/exercisesjavafx/fxml/AppFXML.java
@@ -0,0 +1,30 @@
+package org.example.exercisesjavafx.fxml;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AppFXML extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        String cssFile = getClass().getResource("/fxml/Login.css").toExternalForm();
+        URL fxmlFile = getClass().getResource("/fxml/Login.fxml");
+        GridPane root = FXMLLoader.load(fxmlFile);
+
+        Scene scene = new Scene(root, 350, 350);
+        scene.getStylesheets().add(cssFile);
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Login Screen");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
