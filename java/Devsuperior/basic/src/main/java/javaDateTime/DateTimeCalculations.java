89bff97698422e90d1ea116d5dd5570791d529d8
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-06-11 19:39:20 -0300

    Special topics in java: date-time

---
+++ java/Devsuperior/basic/src/main/java/javaDateTime/DateTimeCalculations.java
@@ -0,0 +1,43 @@
+package javaDateTime;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class DateTimeCalculations {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2024-06-10");
+        LocalDateTime d05 = LocalDateTime.parse("2024-06-10T21:30:26");
+        Instant d06 = Instant.parse("2024-06-10T21:30:26z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+        System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+        System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        Duration t4 = Duration.between(d06, pastWeekInstant);
+
+        System.out.println("t1 days = " + t1.toDays());
+        System.out.println("t2 days = " + t2.toDays());
+        System.out.println("t2 days = " + t3.toDays());
+        System.out.println("t2 days = " + t4.toDays());
+    }
+}
