ffb8a89c90e496389dd929f76a7b900c2c7afa16
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-07-04 21:04:07 -0300

    Java Function

--- java/Devsuperior/basic/src/main/java/generics/map/application/Program.java
+++ java/Devsuperior/basic/src/main/java/generics/map/application/Program.java
@@ -7,6 +7,9 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Program {
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
     public static void main(String[] args) {
 
         List<Product> list = new ArrayList<>();
@@ -14,15 +17,7 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = new Comparator<Product>() {
-
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-            }
-        };
-
-        list.sort(comp);
+        list.sort(Program::compareProducts);
 
         list.forEach(System.out::println);
     }
