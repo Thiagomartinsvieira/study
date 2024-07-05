dc355f493e0eacc9de27d307ec6687a545312100
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-07-04 21:32:20 -0300

    Stream

--- java/Devsuperior/basic/src/main/java/functionalProgramingAndLambdaExpressions/src/application/Program.java
+++ java/Devsuperior/basic/src/main/java/functionalProgramingAndLambdaExpressions/src/application/Program.java
@@ -1,25 +1,30 @@
 package functionalProgramingAndLambdaExpressions.src.application;
 
 import functionalProgramingAndLambdaExpressions.src.entities.Product;
-import functionalProgramingAndLambdaExpressions.src.util.UpperCaseName;
+import functionalProgramingAndLambdaExpressions.src.util.ProductService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
 
-        List<Product> list = new ArrayList<>();
+        List<Integer> list = Arrays.asList(2, 4, 5, 10, 7);
 
-        list.add(new Product("Tv", 900.00));
-        list.add(new Product("Mouse", 50.00));
-        list.add(new Product("Tablet", 350.50));
-        list.add(new Product("HD Case", 80.90));
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st1.toArray()));
 
-        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+        System.out.println(Arrays.toString(st2.toArray()));
 
-        names.forEach(System.out::println);
+        Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
+
+        Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
+        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
     }
 }
