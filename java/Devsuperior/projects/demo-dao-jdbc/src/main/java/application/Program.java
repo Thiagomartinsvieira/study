6171316911077161579e8211462f50634f0fe0bc
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-07-10 21:38:05 -0300

    Delete implementation

--- java/Devsuperior/projects/demo-dao-jdbc/src/main/java/application/Program.java
+++ java/Devsuperior/projects/demo-dao-jdbc/src/main/java/application/Program.java
@@ -1,20 +1,39 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
-
 public class Program {
+
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("==== TEST 1: seller findById ====");
+        System.out.println("=== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment =====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll =====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 3: seller findAll =====");
+        Seller newSeller = new Seller(null, "Vitoria", "vitoria@email.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
     }
 }
