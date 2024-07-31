ada82ba3b27ae58c655fc733096616d97ca375fe
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-07-31 14:35:13 -0300

    Limitations for TextField, Initializable interface

---
+++ java/Devsuperior/javaFx/javaFx5/src/main/java/org/example/javafx1/HelloApplication.java
@@ -0,0 +1,27 @@
+package org.example.javafx1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/org/example/javafx1/gui/View.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
